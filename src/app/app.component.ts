26c11aa79ef33c62581f8bfa1e6e271ea079f887
Jimmy Salazar <jimmyr.salazar@gmail.com>
2021-10-25 14:45:23 -0500

    Added Currency pipe table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-pipes';
+  a: number = 0.26;
+  b: number = 1.3495;
 }
